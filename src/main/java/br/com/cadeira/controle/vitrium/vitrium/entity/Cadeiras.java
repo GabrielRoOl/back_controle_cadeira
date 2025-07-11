28e25d1df25c12102c3ddac8fb2a505e273358cd
GabrielRoOl <gabriel.ro.ol@hotmail.com>
2025-07-11 18:44:03 -0300

    feat: add entity Cadeiras

--- src/main/java/br/com/cadeira/controle/vitrium/vitrium/entity/Cadeiras.java
+++ src/main/java/br/com/cadeira/controle/vitrium/vitrium/entity/Cadeiras.java
@@ -1,22 +1,14 @@
 package br.com.cadeira.controle.vitrium.vitrium.entity;
 
-import java.time.Instant;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import br.com.cadeira.controle.vitrium.vitrium.entity.enums.ECadeira;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.Instant;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -35,21 +27,37 @@ public class Cadeiras {
 
     @Column(name = "Nome_Clinica")
     private String destino;
-    
+
     @Column(name = "Numero_Sala")
     private int nmrClinica;
-    
+
     @Column(name = "Data_Entrega")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC-3")
     private Instant dtEntrega;
-    
+
     @Column(name = "Data_Devolucao")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC-3")
     private Instant dtDevolucao;
-    
+
     @Enumerated(EnumType.STRING)
     private ECadeira cadeira;
 
-    private Boolean devolvida;
-    
+    private Boolean devolvida = false;
+
+    public Cadeiras(String nomePaciente, String destino, int nmrClinica, ECadeira cadeira) {
+        this.nomePaciente = nomePaciente;
+        this.destino = destino;
+        this.nmrClinica = nmrClinica;
+        this.cadeira = cadeira;
+    }
+
+    public Cadeiras(String nomePaciente, String destino, int nmrClinica) {
+        this.nomePaciente = nomePaciente;
+        this.destino = destino;
+        this.nmrClinica = nmrClinica;
+    }
+
+    public void registraHorarioEntrega() {
+        this.dtEntrega = Instant.now();
+    }
 }
