4957b2235d9dc3756eee6cd1100d23e8391f5eee
GabrielRoOl <gabriel.ro.ol@hotmail.com>
2025-07-19 18:16:31 -0300

    feat: add validations

--- src/main/java/br/com/cadeira/controle/vitrium/vitrium/dto/ListaCadeiraPorIdDTO.java
+++ src/main/java/br/com/cadeira/controle/vitrium/vitrium/dto/ListaCadeiraPorIdDTO.java
@@ -1,15 +1,17 @@
 package br.com.cadeira.controle.vitrium.vitrium.dto;
 
 import br.com.cadeira.controle.vitrium.vitrium.entity.enums.ECadeira;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.OffsetDateTime;
 
 public record ListaCadeiraPorIdDTO(
-        String nomePaciente,
-        String destino,
-        int numeroClinica,
+        @NotBlank String nomePaciente,
+        @NotBlank String destino,
+        @NotNull int numeroClinica,
         OffsetDateTime dataEntrega,
         OffsetDateTime dataDevolucao,
-        ECadeira cadeira,
+        @NotNull ECadeira cadeira,
         Boolean devolvida) {
 }
