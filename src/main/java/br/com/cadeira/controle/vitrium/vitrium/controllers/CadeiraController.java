dc8e1e55514b99310109a05a8ba21ae3b2f47c7a
GabrielRoOl <gabriel.ro.ol@hotmail.com>
2025-07-27 08:11:30 -0300

    feat: update endpoint /api/cadeira and add dependency apiconf for swagger

--- src/main/java/br/com/cadeira/controle/vitrium/vitrium/controllers/CadeiraController.java
+++ src/main/java/br/com/cadeira/controle/vitrium/vitrium/controllers/CadeiraController.java
@@ -17,7 +17,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/cadeira")
+@RequestMapping(value = "api/cadeira")
 public class CadeiraController {
 
     @Autowired
