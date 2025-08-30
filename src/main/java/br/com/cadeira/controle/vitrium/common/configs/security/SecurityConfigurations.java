df97efa1667aa2d803ce9519da887d4d646bba07
GabrielRoOl <gabriel.ro.ol@hotmail.com>
2025-08-30 17:59:25 -0300

    config: add security configurations

--- src/main/java/br/com/cadeira/controle/vitrium/common/configs/security/SecurityConfigurations.java
+++ src/main/java/br/com/cadeira/controle/vitrium/common/configs/security/SecurityConfigurations.java
@@ -1,4 +1,44 @@
-package br.com.cadeira.controle.vitrium.authentication.infra.security;
+package br.com.cadeira.controle.vitrium.common.configs.security;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                // Ativando sessão statiles
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Permite todas as requisições, sem exceção
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager getAuthenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // faz a criptografia das senhas
+    }
 }
